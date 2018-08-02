365b839c225a2536f1237c9719e3b45bb5e70126
XSY <xsy@mail.cstu.gq>
2018-08-02 16:15:13 +0000

    XSY-Update-1

---
+++ about.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>About - CCS</title>
+    <link rel="shortcut icon" href="./logo.png">
+    <link rel="bookmark" href="./logo.png">
+    <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+    <script src="./layui/layui.js"></script>
+    <script src="./js/language.js"></script>
+    <link rel="stylesheet" href="./layui/css/layui.css">
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+<br><br><br><br>
+<div class="layui-container">  
+<div class="layui-row">
+    <h1>时间轴<h1>
+      <br><br>
+<ul class="layui-timeline">
+    <li class="layui-timeline-item">
+        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+        <div class="layui-timeline-content layui-text">
+            <h3 class="layui-timeline-title">2015年6月20日</h3>
+            <p>
+                Crystal Computer Studio成立<i class="layui-icon"></i>
+            </p>
+        </div>
+    </li>
+    <li class="layui-timeline-item">
+        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+        <div class="layui-timeline-content layui-text">
+            <h3 class="layui-timeline-title">2018年1月</h3>
+            <p>
+                Crystal Computer Studio第一次在CTS发题
+            </p>
+        </div>
+    </li>
+    <li class="layui-timeline-item">
+        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+        <div class="layui-timeline-content layui-text">
+            <h3 class="layui-timeline-title">2018年10月</h3>
+            <p>
+                经历艰险，Crystal Computer Studio有了第一台自己的服务器！
+            </p>
+        </div>
+    </li>
+    <li class="layui-timeline-item">
+        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+        <div class="layui-timeline-content layui-text">
+            <h3 class="layui-timeline-title">2018年6月21日</h3>
+            <p>
+                Crystal Computer Studio召开第一场组内会议！
+            </p>
+        </div>
+    </li>
+    <li class="layui-timeline-item">
+        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+        <div class="layui-timeline-content layui-text">
+            <h3 class="layui-timeline-title">2018年8月3日</h3>
+            <p>
+                Crystal Computer Studio网站1.0发布，并且成功上线！
+            </p>
+        </div>
+    </li>
+    <li class="layui-timeline-item">
+        <i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+        <div class="layui-timeline-content layui-text">
+            <h3 class="layui-timeline-title">未来。。。</h3>
+        </div>
+    </li>
+</ul>
+  </div>  </div>   
+</body>
+<!-- 一言获取 -->
+<footer>
+    <div class="layui-col-md12">
+        <div class="s-footer">
+            <ul>
+                <li><strong>XSY</strong></li>
+                <li>Version: v1.0</li>
+                <li id="hitokoto">:D 获取中...</li>
+            </ul>
+        </div>
+    </div>
+</footer>
+</html>
