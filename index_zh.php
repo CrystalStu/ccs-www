e3dfd630644f76c0331bcc727facf4c8fe9890bd
XSY <xsy@mail.cstu.gq>
2018-08-03 13:31:21 +0000

    XSY-Update-我也不记得多少了

--- index_zh.php
+++ index_zh.php
@@ -1,4 +1,20 @@
-<?php include $_SERVER['DOCUMENT_ROOT']."/header.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>CCS - Crystal Studios</title>
+    <link rel="shortcut icon" href="./logo.png">
+    <link rel="bookmark" href="./logo.png">
+    <script src="//v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+    <script src="/layui/layui.js"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/css/mdui.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
+    <link rel="stylesheet" href="/layui/css/layui.css">
+    <link rel="stylesheet" href="/css/index.css">
+    <script>var lang = "zh";</script>
+</head>
+<body>
+
 <!-- 头 -->
 <div class="layui-container">
     <div class="layui-row">
@@ -182,4 +198,53 @@
                 </div>
             </div>
         </div>
-<?php include $_SERVER['DOCUMENT_ROOT']."/footer.php"; ?>
+
+        <!-- 侧边按钮 -->
+        <script>
+            layui.use(['util',  'layer'], function(){
+                var util = layui.util
+                    ,layer = layui.layer;
+                //执行
+                util.fixbar({
+                    bar1: "&#xe614"
+                    ,click: function(type){
+                        console.log(type);
+                        if(type === 'bar1'){
+                            layer.open({
+                                type: 1,
+                                title:'Language',
+                                skin: 'layui-layer-molv', //样式类名
+                                closeBtn: 1, //显示关闭按钮
+                                anim: 2,
+                                area: ['250px', '200px'],
+                                shadeClose: true, //开启遮罩关闭
+                                content: '<ul class="mdui-list">\n' +
+                                    '  <li class="mdui-list-item mdui-ripple" onclick="changeLang_newDesign_en();">English</li>\n' +
+                                    '  <li class="mdui-list-item mdui-ripple" onclick="changeLang_newDesign_ja();">日本語</li>\n' +
+                                    '  <li class="mdui-list-item mdui-ripple" onclick="changeLang_newDesign_zh();">中文</a></li>\n' +
+                                    '</ul>'
+                            });
+                        }
+                    }
+                });
+            });
+        </script>
+
+
+        <!-- footer -->
+        <footer>
+            <script src="/js/language.js"></script>
+            <div class="layui-col-md12">
+                <div class="s-footer">
+                    <ul>
+                        <li><strong>Powered by &copy; Crystal Web Service & XSY</strong></li>
+                        <li>Version <?php include $_SERVER['DOCUMENT_ROOT']."/ver.php"; ?></li>
+                        <li id="hitokoto">:D 获取中...</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+</body>
+</html>
+
