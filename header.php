0ecbf441e751615266ad4946d12d5b87f1decb7a
XSY <xsy@mail.cstu.gq>
2018-08-03 14:19:57 +0000

    修复header的一点小瑕疵

--- header.php
+++ header.php
@@ -2,20 +2,18 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>CCS - Crystal Studios</title>
-    <link rel="shortcut icon" href="./logo.png">
-    <link rel="bookmark" href="./logo.png">
+    <link rel="shortcut icon" href="logo.png">
+    <link rel="bookmark" href="logo.png">
     <script src="https://v1.hitokoto.cn/?encode=js&select=%23hitokoto" defer></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
     <script src="/layui/layui.js"></script>
     <link href="//fonts.googleapis.com/css?family=Open+Sans|Ubuntu+Mono" rel="stylesheet">
     <link href="//fonts.googleapis.com/earlyaccess/notosansjp.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/earlyaccess/notosanstc.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/earlyaccess/notosanssc.css" rel="stylesheet">
     <link href="//fonts.googleapis.com/earlyaccess/notosanskr.css" rel="stylesheet">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/css/mdui.min.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/js/mdui.min.js"></script>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.1/css/mdui.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/layui/css/layui.css">
     <link rel="stylesheet" href="/css/index.css">
-    <script>var lang = "zh";</script>
 </head>
 <body>
